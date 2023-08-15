029baf373de46a3453472514c717e49c8f8733cf
Christine Baik <christuaik@gmail.com>
2023-08-15 15:46:28 -0700

    complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,9 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge = ['math']):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
